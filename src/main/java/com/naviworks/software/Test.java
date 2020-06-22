d5126fb2cc5096485a6f4c04d602b4ce7f9fbc90
heeyoungJung <12young24@naver.com>
2020-06-22 14:12:56 +0900

    init

---
+++ src/main/java/com/naviworks/software/Test.java
@@ -0,0 +1,39 @@
+package com.naviworks.software;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private int age;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/com/naviworks/software/TestRepository.java
@@ -0,0 +1,8 @@
+package com.naviworks.software;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface TestRepository extends
+        PagingAndSortingRepository<Test, Long> {
+
+}
